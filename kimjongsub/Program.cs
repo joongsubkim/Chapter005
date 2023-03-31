66cba8522440ef781adcacd22f1cee7d88b34694
kimjongsub <nomjr@daum.net>
2023-03-31 20:06:00 +0900

    202303311005

---
+++ kimjongsub/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+namespace kimjongsub_ex1
+{
+    class mainapp
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ kimjongsub_ex2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace kimjongsub_ex2
+{
+    class mainapp
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 5; i > 0; i--)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
+
+
